a4bc3ceabb1bf1b249746af23e4e41bb4603586c
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-15 19:04:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,15 @@
 import streamlit as st
 
+st.set_page_config(layout="wide")
+
 st.title('How to layout your Streamlit app')
+
+st.sidebar.header('Input')
+user_name = st.sidebar.text_input('What is your name?')
+
+st.header('Output')
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+  st.write(f'Hello {user_name}')
