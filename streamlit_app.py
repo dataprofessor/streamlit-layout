b068aeffda39b4fefd91f119ab3f147fd7f34437
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-15 19:18:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.title('How to layout your Streamlit app')
 
 st.sidebar.header('Input')
 user_name = st.sidebar.text_input('What is your name?')
-user_emoji = st.sidebar.selectbox('How do you feel?', [😄, 😆, 😊, 😍, 😴, 😕, 😱])
+user_emoji = st.sidebar.selectbox('How do you feel?', ['happy', 'tired', 'excited'])
 
 st.header('Output')
 
